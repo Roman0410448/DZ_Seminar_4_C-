36991c3ad3520c36a122b597221b37cb26f1cb1d
ROMAN <junk0410448@mail.ru>
2023-03-22 23:32:28 +0700

    Z1

---
+++ Z_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+ for(int i=0; i<1;i++)
+ {
+    Console.Write (Math.Pow(A,B));
+ }
