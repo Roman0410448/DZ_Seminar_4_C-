2a1fcf59dd8f7936647f2f0d7cc5b011ff9fc56b
ROMAN <junk0410448@mail.ru>
2023-03-23 11:44:30 +0700

    Z3

--- Z_2/Program.cs
+++ Z_2/Program.cs
@@ -1,4 +1,8 @@
-﻿// Функция ввода
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// Функция ввода
 int ReadInt(string message)
 {
     Console.Write(message);
@@ -11,7 +15,7 @@ int[] randomArray = new int[lenArray];
 for (int i = 0; i < randomArray.Length; i++)
 {
     randomArray[i] = new Random().Next(1,100);
-    Console.Write(randomArray[i] + ", ");
+    Console.Write(randomArray[i] + ",");
 }
 
 
