f59d97d713bc752166824759916393ae71e1e374
ROMAN <junk0410448@mail.ru>
2023-03-23 11:48:17 +0700

    Z 3

--- Z_3/Program.cs
+++ Z_3/Program.cs
@@ -4,16 +4,15 @@
 82 -> 10
 9012 -> 12
 */
-
-int number = ReadInt("Введите число: ");
-int len = NumberLen(number);
-SumNumbers(number, len);
-
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
 
 // Функция подсчета цифр в числе
 int NumberLen(int a)
